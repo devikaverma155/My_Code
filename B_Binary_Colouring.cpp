60488a3e4633923d8fc9caf4407fb37840b32de3
devikaverma155 <devikaverma1554@gmail.com>
2024-06-08 15:16:25 +0530

    8/6

--- B_Binary_Colouring.cpp
+++ B_Binary_Colouring.cpp
@@ -1,32 +1,40 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-#define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-#define endl '\n'
-#define incr_loop(a, n) for (int i = a; i < n; i++)
-#define decr_loop(a, b) for (int i = a; i > b; i--)
-#define nested_incr_loop(a, b) for (int j = a; j < b; j++)
-#define nested_decr_loop(a, b) for (int j = b; j > a; --j)
-typedef long long ll;
-#define pb push_back
-#define ppb pop_back
-#define MP make_pair
-#define vi vector<int>
-#define sort_all(v) sort(all(v));
-#define PI 3.141592653589793238462
-#define space cout << ' ';
-#define gcd(a, b) __gcd(a, b)
+
+void solve() {
+    long long x;
+    cin >> x;
+    vector<int> res(31, 0);
+
+    for (int i = 0; i < 30; i++) {
+        if (x & (1LL << i)) {
+            if (res[i] == 1) {
+                res[i] = 0;
+                res[i + 1] = 1;
+            } else if (i > 0 && res[i - 1] == 1) {
+                res[i - 1] = -1;
+                res[i + 1] = 1;
+            } else {
+                res[i] = 1;
+            }
+        }
+    }
+
+    cout << 31 << '\n';
+    for (int i = 0; i <= 30; i++) {
+        cout << res[i] << ' ';
+    }
+    cout << '\n';
+}
 
 int main() {
-    fio
-    int t;
-    cin >> t;
-    while (t--) {
-int n;
-cin>>n;
-vi v(n);
-for( int i=0;i<n;i++){
-cin>>v[i];
-}
+    int t;cin>>t;
+    while (t--)
+    {
+       solve();
     }
+    
+    
     return 0;
 }
